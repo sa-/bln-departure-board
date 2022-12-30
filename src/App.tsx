580ded3e86de08719c4953f480aa3c7c98da1f9f
Samay Kapadia <samay.kapadia@gmail.com>
2022-12-30 14:50:26 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css';
-import axios from 'axios';
-import { useState } from 'react';
 import DisplayBoard from './DisplayBoard'
 import React from 'react';
 
@@ -16,18 +14,11 @@ class LatLong  {
 }
 
 function App() {
-  let location = getLocation();
-
   return (
     <div className="App">
       <DisplayBoard />
-      
     </div>
   );
-}
-
-function getLocation() {
-  return new LatLong(52.5066846400927, 13.44972267216406)
 }
 
 export default App;
--- src/DisplayBoard.tsx
+++ src/DisplayBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 
 interface DisplayBoardProps extends React.HTMLProps<React.HTMLInputTypeAttribute> { }
